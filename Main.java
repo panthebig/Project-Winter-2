3604c0e1196b5beb1924b6b7a134d4e19bef66cf
panthebig <47485013+panthebig@users.noreply.github.com>
2020-11-13 13:36:30 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.pakpan.pro;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println(15);
+        System.out.println(25);
+        Game a = new Game();
+        a.output();
+
+    }
+
+}
