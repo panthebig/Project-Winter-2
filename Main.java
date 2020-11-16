55b165f94e7678a61c14d18efbb96a801b89e54f
pfilippos <pfilippos@csd.auth.gr>
2020-11-16 17:12:55 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,11 @@
-import java.util.Scanner;
-
-
 public class Main {
     public static void main(String[] args){
 
+        public static void main(String[] args) {
+            System.out.println("Hello, let's play Buzz!\n");
+        }
         Game NewGame = new Game();
         NewGame.GameStart();
-
-        //Scanner read = new Scanner(System.in);
-        //int number = read.nextInt();
-
 
     }
 }
